5800c556f9a3ec71fc07f98cf9298c0729b255f0
Yousef <yayalkandari@gmail.com>
2025-02-18 19:41:29 +0300

    added loop to list all roles

--- src/Main.kt
+++ src/Main.kt
@@ -6,6 +6,9 @@ fun main() {
     println("Random dudes have perms: ${hasEditPerm(someRandomDude)}")
     println("Editors have perms: ${hasEditPerm(myBoss)}")
     println("Admins have perms: ${hasEditPerm(superUser)}")
+
+    println("Available permissions:")
+    UserRole.entries.forEach { println("\t[-] Permission Level: ${it.name} - Access Level ${it.accessLevel}") }
 }
 
 enum class UserRole(val accessLevel: Int) {
